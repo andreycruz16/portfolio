97478d01d3010eda262907962b9422af6fc10d89
Mark Andrey <markandreydelacruz@ymail.com>
2023-03-03 23:27:17 +0800

    install tailwindcss

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,8 @@
-import '@/styles/App.scss';
-
 function App() {
     return (
         <div className="App">
-            <div>Hello World</div>
+            <div className="text-orange-700">Hello World</div>
+            <h1 className="text-3xl text-orange-300 underline">Hello worlds!</h1>
         </div>
     );
 }
